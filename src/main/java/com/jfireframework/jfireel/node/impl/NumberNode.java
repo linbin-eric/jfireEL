42c2c905111e844547379681e483aaea487929e0
eric <eric@jfire.cn>
2018-05-16 09:56:12 +0800

    修正一个错误的Float静态方法调用，该方法于jdk1.8才出现

--- src/main/java/com/jfireframework/jfireel/node/impl/NumberNode.java
+++ src/main/java/com/jfireframework/jfireel/node/impl/NumberNode.java
@@ -7,47 +7,47 @@ import com.jfireframework.jfireel.token.Expression;
 
 public class NumberNode implements CalculateNode
 {
-    private Number value;
-    
-    public NumberNode(String literals)
-    {
-        if (literals.indexOf('.') > -1)
-        {
-            value = Float.valueOf(literals);
-            if (Float.isFinite((Float) value) == false)
-            {
-                value = Double.valueOf(literals);
-            }
-        }
-        else
-        {
-            try
-            {
-                value = Integer.valueOf(literals);
-            }
-            catch (NumberFormatException e)
-            {
-                value = Long.valueOf(literals);
-            }
-        }
-    }
-    
-    @Override
-    public Object calculate(Map<String, Object> variables)
-    {
-        return value;
-    }
-    
-    @Override
-    public CalculateType type()
-    {
-        return Expression.NUMBER;
-    }
-    
-    @Override
-    public String toString()
-    {
-        return "NumberNode [value=" + value + "]";
-    }
-    
+	private Number value;
+	
+	public NumberNode(String literals)
+	{
+		if (literals.indexOf('.') > -1)
+		{
+			value = Float.valueOf(literals);
+			if (Float.isInfinite((Float) value))
+			{
+				value = Double.valueOf(literals);
+			}
+		}
+		else
+		{
+			try
+			{
+				value = Integer.valueOf(literals);
+			}
+			catch (NumberFormatException e)
+			{
+				value = Long.valueOf(literals);
+			}
+		}
+	}
+	
+	@Override
+	public Object calculate(Map<String, Object> variables)
+	{
+		return value;
+	}
+	
+	@Override
+	public CalculateType type()
+	{
+		return Expression.NUMBER;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "NumberNode [value=" + value + "]";
+	}
+	
 }
