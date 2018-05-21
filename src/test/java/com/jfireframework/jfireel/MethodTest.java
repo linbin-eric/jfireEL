ceffcdbf163f21f93d69e67a7faa2220d0001d2c
eric <eric@jfire.cn>
2018-05-21 17:56:51 +0800

    增加了BeetlEl的性能对比。

--- src/test/java/com/jfireframework/jfireel/MethodTest.java
+++ src/test/java/com/jfireframework/jfireel/MethodTest.java
@@ -3,6 +3,13 @@ package com.jfireframework.jfireel;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import java.io.IOException;
+import java.util.Map;
+import org.beetl.core.Configuration;
+import org.beetl.core.GroupTemplate;
+import org.beetl.core.Template;
+import org.beetl.core.exception.ScriptEvalError;
+import org.beetl.core.resource.StringTemplateResourceLoader;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.expression.Expression;
@@ -128,8 +135,7 @@ public class MethodTest extends TestSupport
 	}
 	
 	@Test
-	@Ignore
-	public void perTest()
+	public void perTest() throws IOException, ScriptEvalError
 	{
 		String value = person.age + "12";
 		vars.put("value", value);
@@ -138,15 +144,22 @@ public class MethodTest extends TestSupport
 		Expression exp = parser.parseExpression("vars['home'].bool(vars['person'].getAge() + '12' != vars['value'])");
 		Object springElResult = exp.getValue(societyContext);
 		System.out.println(springElResult);
+		StringTemplateResourceLoader resourceLoader = new StringTemplateResourceLoader();
+		Configuration cfg = Configuration.defaultConfiguration();
+		GroupTemplate gt = new GroupTemplate(resourceLoader, cfg);
+		Map runScript = gt.runScript("return @home.bool(@person.getAge()+'12'!=value);", vars);
+		System.out.println(runScript);
+		
 		Lexer lexer = Lexer.parse("home.bool(person.getAge() + '12' != value)");
 		Lexer lexer2 = Lexer.parse("home.bool(person.getAge() + '12' != value)", Functional.build().setMethodInvokeByCompile(true).toFunction());
-		int preheat = 100000;
+		int preheat = 1000;
 		int count = 10000000;
 		for (int i = 0; i < preheat; i++)
 		{
 			lexer.calculate(vars);
 			lexer2.calculate(vars);
 			exp.getValue(societyContext);
+			gt.runScript("return @home.bool(@person.getAge()+'12'!=value);", vars);
 		}
 		long t0 = System.nanoTime();
 		for (int i = 0; i < count; i++)
@@ -162,6 +175,13 @@ public class MethodTest extends TestSupport
 		}
 		t1 = System.nanoTime();
 		System.out.println("编译优化jfireEl计算" + (count / 10000) + "万次耗时:" + (t1 - t0) / 1000 / 1000 + "毫秒");
+		t0 = System.nanoTime();
+		for (int i = 0; i < count; i++)
+		{
+			gt.runScript("return @home.bool(@person.getAge()+'12'!=value);", vars);
+		}
+		t1 = System.nanoTime();
+		System.out.println("BeetlEl计算" + (count / 10000) + "万次耗时:" + (t1 - t0) / 1000 / 1000 + "毫秒");
 		t0 = System.nanoTime();
 		for (int i = 0; i < count; i++)
 		{
