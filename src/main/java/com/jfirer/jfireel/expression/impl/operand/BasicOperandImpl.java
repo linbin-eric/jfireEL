218782dc65228bc29172f0a8d0c934781c170b62
linbin <495561397@qq.com>
2023-12-18 01:21:00 +0800

    继续重构四则运算

--- src/main/java/com/jfirer/jfireel/expression/impl/operand/BasicOperandImpl.java
+++ src/main/java/com/jfirer/jfireel/expression/impl/operand/BasicOperandImpl.java
@@ -91,6 +91,95 @@ public abstract class BasicOperandImpl implements Operand
         }
     }
 
+    interface MathOp1
+    {
+        Object math(long l, long r);
+    }
+
+    interface MathOp2
+    {
+        Object math(double l, double r);
+    }
+
+    interface MathOp3
+    {
+        Object math(long l, double r);
+    }
+
+    interface MathOp4
+    {
+        Object math(double l, long r);
+    }
+
+    protected abstract class MathOperand extends BasicOperandImpl
+    {
+        MathOp1 op1;
+        MathOp2 op2;
+        MathOp3 op3;
+        MathOp4 op4;
+
+        public MathOperand(Operand left, Operand right, String fragment, MathOp1 mathOp1, MathOp2 mathOp2, MathOp3 mathOp3, MathOp4 mathOp4)
+        {
+            super(left, right, fragment);
+            this.op1 = mathOp1;
+            this.op2 = mathOp2;
+            this.op3 = mathOp3;
+            this.op4 = mathOp4;
+        }
+
+        @Override
+        public Object calculate(Map<String, Object> contextParam)
+        {
+            Object leftValue  = left.calculate(contextParam);
+            Object rightValue = right.calculate(contextParam);
+            if (leftValue instanceof Number && rightValue instanceof Number)
+            {
+                if (leftValue instanceof Byte || leftValue instanceof Short || leftValue instanceof Integer || leftValue instanceof Long)
+                {
+                    long l = ((Number) leftValue).longValue();
+                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer || rightValue instanceof Long)
+                    {
+                        return op1.math(l, ((Number) rightValue).longValue());
+                    }
+                    else if (rightValue instanceof Float || rightValue instanceof Double)
+                    {
+                        return op3.math(l, ((Number) rightValue).doubleValue());
+                    }
+                    else
+                    {
+                        throw new IllegalArgumentException();
+                    }
+                }
+                else if (leftValue instanceof Float || leftValue instanceof Double)
+                {
+                    double v = ((Number) leftValue).doubleValue();
+                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer || rightValue instanceof Long)
+                    {
+                        return op4.math(v, ((Number) rightValue).longValue());
+                    }
+                    else if (rightValue instanceof Float || rightValue instanceof Double)
+                    {
+                        return op2.math(v, ((Number) rightValue).doubleValue());
+                    }
+                    else
+                    {
+                        throw new IllegalArgumentException();
+                    }
+                }
+                else
+                {
+                    throw new IllegalArgumentException();
+                }
+            }
+            else
+            {
+                return processOther(leftValue, rightValue);
+            }
+        }
+
+        protected abstract Object processOther(Object leftValue, Object rightValue);
+    }
+
     public static class PlusOperand extends BasicOperandImpl
     {
         private static final ThreadLocal<StringBuilder> BUILDER = ThreadLocal.withInitial(() -> new StringBuilder());
@@ -114,85 +203,41 @@ public abstract class BasicOperandImpl implements Operand
             }
             else if (leftValue instanceof Number && rightValue instanceof Number)
             {
-                if (leftValue instanceof Byte || leftValue instanceof Short || leftValue instanceof Integer)
-                {
-                    int i = ((Number) leftValue).intValue();
-                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer)
-                    {
-                        return i + ((Number) rightValue).intValue();
-                    }
-                    else if (rightValue instanceof Long)
-                    {
-                        return i + ((Long) rightValue).longValue();
-                    }
-                    else if (rightValue instanceof Float)
-                    {
-                        return i + ((Float) rightValue).floatValue();
-                    }
-                    else
-                    {
-                        return i + ((Number) rightValue).doubleValue();
-                    }
-                }
-                else if (leftValue instanceof Long)
+                if (leftValue instanceof Byte || leftValue instanceof Short || leftValue instanceof Integer || leftValue instanceof Long)
                 {
                     long i = ((Number) leftValue).longValue();
-                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer)
+                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer || rightValue instanceof Long)
                     {
-                        return i + ((Number) rightValue).intValue();
+                        return i + ((Number) rightValue).longValue();
                     }
-                    else if (rightValue instanceof Long)
+                    else if (rightValue instanceof Float || rightValue instanceof Double)
                     {
-                        return i + ((Long) rightValue).longValue();
-                    }
-                    else if (rightValue instanceof Float)
-                    {
-                        return i + ((Float) rightValue).floatValue();
+                        return i + ((Number) rightValue).doubleValue();
                     }
                     else
                     {
-                        return i + ((Number) rightValue).doubleValue();
+                        throw new IllegalArgumentException();
                     }
                 }
-                else if (leftValue instanceof Float)
+                else if (leftValue instanceof Float || leftValue instanceof Double)
                 {
-                    float i = ((Number) leftValue).floatValue();
-                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer)
+                    double i = ((Number) leftValue).doubleValue();
+                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer || rightValue instanceof Long)
                     {
-                        return i + ((Number) rightValue).intValue();
+                        return i + ((Number) rightValue).longValue();
                     }
-                    else if (rightValue instanceof Long)
+                    else if (rightValue instanceof Float || rightValue instanceof Double)
                     {
-                        return i + ((Long) rightValue).longValue();
-                    }
-                    else if (rightValue instanceof Float)
-                    {
-                        return i + ((Float) rightValue).floatValue();
+                        return i + ((Number) rightValue).doubleValue();
                     }
                     else
                     {
-                        return i + ((Number) rightValue).doubleValue();
+                        throw new IllegalArgumentException();
                     }
                 }
                 else
                 {
-                    double i = ((Number) leftValue).doubleValue();
-                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer)
-                    {
-                        return i + ((Number) rightValue).intValue();
-                    }
-                    else if (rightValue instanceof Long)
-                    {
-                        return i + ((Long) rightValue).longValue();
-                    }
-                    else if (rightValue instanceof Float)
-                    {
-                        return i + ((Float) rightValue).floatValue();
-                    }
-                    else
-                    {
-                        return i + ((Number) rightValue).doubleValue();
-                    }
+                    throw new IllegalArgumentException();
                 }
             }
             else
@@ -216,85 +261,41 @@ public abstract class BasicOperandImpl implements Operand
             Object rightValue = right.calculate(contextParam);
             if (leftValue instanceof Number && rightValue instanceof Number)
             {
-                if (leftValue instanceof Byte || leftValue instanceof Short || leftValue instanceof Integer)
-                {
-                    int i = ((Number) leftValue).intValue();
-                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer)
-                    {
-                        return i - ((Number) rightValue).intValue();
-                    }
-                    else if (rightValue instanceof Long)
-                    {
-                        return i - ((Long) rightValue).longValue();
-                    }
-                    else if (rightValue instanceof Float)
-                    {
-                        return i - ((Float) rightValue).floatValue();
-                    }
-                    else
-                    {
-                        return i - ((Number) rightValue).doubleValue();
-                    }
-                }
-                else if (leftValue instanceof Long)
+                if (leftValue instanceof Byte || leftValue instanceof Short || leftValue instanceof Integer || leftValue instanceof Long)
                 {
                     long i = ((Number) leftValue).longValue();
-                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer)
+                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer || rightValue instanceof Long)
                     {
-                        return i - ((Number) rightValue).intValue();
+                        return i - ((Number) rightValue).longValue();
                     }
-                    else if (rightValue instanceof Long)
+                    else if (rightValue instanceof Float || rightValue instanceof Double)
                     {
-                        return i - ((Long) rightValue).longValue();
-                    }
-                    else if (rightValue instanceof Float)
-                    {
-                        return i - ((Float) rightValue).floatValue();
+                        return i - ((Number) rightValue).doubleValue();
                     }
                     else
                     {
-                        return i - ((Number) rightValue).doubleValue();
+                        throw new IllegalArgumentException();
                     }
                 }
-                else if (leftValue instanceof Float)
+                else if (leftValue instanceof Float || leftValue instanceof Double)
                 {
-                    float i = ((Number) leftValue).floatValue();
-                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer)
+                    double i = ((Number) leftValue).doubleValue();
+                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer || rightValue instanceof Long)
                     {
-                        return i - ((Number) rightValue).intValue();
+                        return i - ((Number) rightValue).longValue();
                     }
-                    else if (rightValue instanceof Long)
+                    else if (rightValue instanceof Float || rightValue instanceof Double)
                     {
-                        return i - ((Long) rightValue).longValue();
-                    }
-                    else if (rightValue instanceof Float)
-                    {
-                        return i - ((Float) rightValue).floatValue();
+                        return i - ((Number) rightValue).doubleValue();
                     }
                     else
                     {
-                        return i - ((Number) rightValue).doubleValue();
+                        throw new IllegalArgumentException();
                     }
                 }
                 else
                 {
-                    double i = ((Number) leftValue).doubleValue();
-                    if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer)
-                    {
-                        return i - ((Number) rightValue).intValue();
-                    }
-                    else if (rightValue instanceof Long)
-                    {
-                        return i - ((Long) rightValue).longValue();
-                    }
-                    else if (rightValue instanceof Float)
-                    {
-                        return i - ((Float) rightValue).floatValue();
-                    }
-                    else
-                    {
-                        return i - ((Number) rightValue).doubleValue();
-                    }
+                    throw new IllegalArgumentException();
                 }
             }
             else
@@ -746,8 +747,8 @@ public abstract class BasicOperandImpl implements Operand
         public GeOperand(Operand left, Operand right, String fragment)
         {
             super(left, right, fragment);
-
         }
+
         @Override
         public Object calculate(Map<String, Object> contextParam)
         {
@@ -848,8 +849,8 @@ public abstract class BasicOperandImpl implements Operand
         public GtOperand(Operand left, Operand right, String fragment)
         {
             super(left, right, fragment);
-
         }
+
         @Override
         public Object calculate(Map<String, Object> contextParam)
         {
@@ -950,8 +951,8 @@ public abstract class BasicOperandImpl implements Operand
         public LeOperand(Operand left, Operand right, String fragment)
         {
             super(left, right, fragment);
-
         }
+
         @Override
         public Object calculate(Map<String, Object> contextParam)
         {
@@ -1052,8 +1053,8 @@ public abstract class BasicOperandImpl implements Operand
         public LtOperand(Operand left, Operand right, String fragment)
         {
             super(left, right, fragment);
-
         }
+
         @Override
         public Object calculate(Map<String, Object> contextParam)
         {
@@ -1086,7 +1087,7 @@ public abstract class BasicOperandImpl implements Operand
                     long i = ((Number) leftValue).longValue();
                     if (rightValue instanceof Byte || rightValue instanceof Short || rightValue instanceof Integer)
                     {
-                        return i <  ((Number) rightValue).intValue();
+                        return i < ((Number) rightValue).intValue();
                     }
                     else if (rightValue instanceof Long)
                     {
